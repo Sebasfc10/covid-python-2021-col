598dfb0ffb50276c075e69cf0b43c0d617a9404f
Sebasfc10 <79613872+Sebasfc10@users.noreply.github.com>
2021-11-29 14:10:19 -0500

    first commit

---
+++ taller.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 29 14:07:44 2021
+
+@author: jeha2
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('covid_22_noviembre.csv')
