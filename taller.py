3b17fa75d5b3eb1c1d03862302ed8db44427fd05
Sebasfc10 <79613872+Sebasfc10@users.noreply.github.com>
2021-11-29 14:21:16 -0500

    ejercicio 1

--- taller.py
+++ taller.py
@@ -10,3 +10,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv('covid_22_noviembre.csv')
+data['Sexo'].replace('f','F',inplace=True)
+data['Sexo'].replace('m','M',inplace=True)
+data['Estado'].replace('leve','Leve',inplace=True)
+data['Estado'].replace('LEVE','Leve',inplace=True)
+
+#1
+n = len(data)
+print(f"ejercicio 1: {n}")
