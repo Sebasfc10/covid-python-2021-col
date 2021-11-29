f210fe529a8c890036a8dd98571d25c01c356d26
Sebasfc10 <79613872+Sebasfc10@users.noreply.github.com>
2021-11-29 14:23:11 -0500

    ejercicio 2

--- taller.py
+++ taller.py
@@ -18,3 +18,16 @@ data['Estado'].replace('LEVE','Leve',inplace=True)
 #1
 n = len(data)
 print(f"ejercicio 1: {n}")
+
+#2
+data['Nombre municipio'].replace('puerto colombia','PUERTO COLOMBIA',inplace=True)
+data['Nombre municipio'].replace('puerto COLOMBIA','PUERTO COLOMBIA',inplace=True)
+data['Nombre municipio'].replace('MEDELLiN','MEDELLIN',inplace=True)
+data['Nombre municipio'].replace('Galapa','GALAPA',inplace=True)
+data['Nombre municipio'].replace('momil','MOMIL',inplace=True)
+data['Nombre municipio'].replace('Guepsa','GUEPSA',inplace=True)
+data['Nombre municipio'].replace('barrancabermeja','BARRANCABERMEJA',inplace=True)
+data['Nombre municipio'].replace('Pensilvania','PENSILVANIA',inplace=True)
+data['Nombre municipio'].replace('Anserma','ANSERMA',inplace=True)
+n_m = len(data.groupby('Nombre municipio').size())
+print(f"ejercicio 2: {n_m}")
